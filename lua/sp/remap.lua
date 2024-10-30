58bdf317982a5db66bac82e6b7e2d862ada10c1f
shanepartridge <55316696+shanepartridge@users.noreply.github.com>
2024-10-30 21:19:05 +0000

    harpoon remap, set tab to 4 whitespace

--- lua/sp/remap.lua
+++ lua/sp/remap.lua
@@ -8,3 +8,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 -- yank into system clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+
